190a5a84d9174f78d23b03775279724ae814d544
G070109 <98209070+G070109@users.noreply.github.com>
2022-02-01 15:01:53 -0600

    Update CW.py

--- CW.py
+++ CW.py
@@ -200,7 +200,7 @@ if a:
             elif rival == "WAS":
                 tm_ex = 29
 
-            Loc_def = Pos_vs_def.iloc[[tm_ex], [pr_ex]]
+            Loc_def = Pos_vs_def.iloc[[tm_ex], [8]]
             Loc_def_Ar = np.asarray(Loc_def)
             Ar_Def = np.append(Ar_Def, [Loc_def_Ar])
 
